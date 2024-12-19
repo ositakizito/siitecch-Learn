7fe44c14be85fd67782f95600b3fff7ab686e457
osita kizito <ositakizito4@gmail.com>
2024-12-19 08:09:55 +0100

    footer changes a little

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,38 +6,32 @@ import {
   FaFacebook,
   FaLinkedin,
   FaGlobe,
-  FaGoogle,
-  FaInstagram,
-  FaTiktok
 } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa6';
 export default function Footer() {
   const { theme } = useTheme();
   const youTube = "https://youtube.com/@siitecch?si=ngX7lFMF0IWnU8X0";
   const faceBook = "https://web.facebook.com/profile.php?id=100076062997043";
   const Twitter = "https://x.com/siitecch";
   const linkedIn = "https://www.linkedin.com/in/christopher-osita-46b4b6202/";
-  const google = "";
-  const instaGram = "https://www.instagram.com/siitecch/?next=%2F";
+  const GitHub = "https://github.com/ositakizito";
   return (
     <>
       <footer className={`footer ${theme}`}>
-        <div className="site-name">SIITECCH</div>
+        <div className="site-name"></div>
         <div className="about-page">
           <div className="about-write">
-            Siitecch is a focused learning platform designed to
-            guide aspiring developers in mastering full-stack development using JavaScript, offering a clear, step-by-step path from frontend to backend all in one place.
+           
           </div>
         </div>
         <div className="footer-link">
           <div className="contact">Link Up With Us</div>
           <div className="socials">
-            <button onClick={() => window.open(faceBook, "_blank", "noopener,noreferrer")}><FaFacebook style={{ color: '#4D9FEB', fontSize: '25px' }} /></button>
+            <button onClick={() => window.open(GitHub, "_blank", "noopener,noreferrer")}><FaGithub style={{ color: '#070B15', fontSize: '25px' }} /></button>
             <button onClick={() => window.open(youTube, "_blank", "noopener,noreferrer")}><FaYoutube style={{ color: '#ff0000', fontSize: '25px' }} /></button>
-            <button onClick={() => window.open(Twitter, "_blank", "noopener,noreferrer")}><FaTwitter style={{ color: '#4D9FEB', fontSize: '25px' }} /></button>
             <button onClick={() => window.open(linkedIn, "_blank", "noopener,noreferrer")}><FaLinkedin style={{ color: '#4D9FEB', fontSize: '25px' }} /></button>
-            <button onClick={() => window.open(google, "_blank", "noopener,noreferrer")}><FaGoogle style={{ color: '#792C2D', fontSize: '25px' }} /></button>
-            <button onClick={() => window.open(instaGram, "_blank", "noopener,noreferrer")}><FaInstagram style={{ color: '#792C2D', fontSize: '25px' }} /></button>
-            <button onClick={() => window.open(instaGram, "_blank", "noopener,noreferrer")}><FaTiktok style={{ color: '#000000', fontSize: '25px' }} /></button>
+            <button onClick={() => window.open(Twitter, "_blank", "noopener,noreferrer")}><FaTwitter style={{ color: '#4D9FEB', fontSize: '25px' }} /></button>
+            <button onClick={() => window.open(faceBook, "_blank", "noopener,noreferrer")}><FaFacebook style={{ color: '#4D9FEB', fontSize: '25px' }} /></button>
           </div>
           <div className="help">
             <div className="page-links"><Link to="/" className="linked">Home</Link></div>
